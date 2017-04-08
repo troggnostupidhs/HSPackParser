c399a4e909192875e5150502b47a08c0cbcf63a9
troggnostupidhs <troggnostupidhs@gmail.com>
2017-04-08 03:45:52 -0400

    PrintDupesByUser - the code is somewhat bad

--- src/Data/Queriers/Querier.cs
+++ src/Data/Queriers/Querier.cs
@@ -26,30 +26,47 @@ namespace HSPackParser.Data.Queriers
         {
             Packs = packs;
             AllCards = packs.Select((p) => p.Cards)
-                            .Aggregate(new List<Card>(), (result, cards) =>
-                            {
-                                result.AddRange(cards);
-                                return result;
-                            }).ToList();
+                .Aggregate(new List<Card>(), (result, cards) =>
+                {
+                    result.AddRange(cards);
+                    return result;
+                }).ToList();
+        }
+
+        private List<Pack> GetPacks()
+        {
+            var newPacks = new List<Pack>();
+            newPacks.AddRange(Packs);
+            return newPacks;
+        }
+
+        private List<Card> GetAllCards()
+        {
+            var newAllCards = new List<Card>();
+            newAllCards.AddRange(AllCards);
+            return newAllCards;
         }
 
         private void PrintByCardCounts()
         {
+            List<Pack> packs = GetPacks();
+            List<Card> allCards = GetAllCards();
+
             var cardCount = new Dictionary<HearthDb.Card, int>();
-            foreach (Card card in AllCards)
+            allCards.ForEach((card) =>
             {
                 HearthDb.Card key = card.CardInfo;
                 int currentCount = cardCount.ContainsKey(key) ? cardCount[key] : 0;
                 currentCount++;
                 cardCount[key] = currentCount;
-            }
+            });
 
             var sorted = cardCount.OrderByDescending(kp => kp.Value).GroupBy(kp => kp.Key.Rarity).ToList();
-            foreach (var groups in sorted)
+            foreach (var group in sorted)
             {
-                var kvps = groups.ToList();
+                var kvps = group.ToList();
 
-                WriteLine("{0} - {1}", groups.Key, kvps.Count());
+                WriteLine("{0} - {1}", group.Key, kvps.Count());
                 WriteLine("------------------------------");
                 kvps.ForEach((kvp) =>
                 {
@@ -59,10 +76,77 @@ namespace HSPackParser.Data.Queriers
             }
         }
 
+        private void PrintDupesByUser()
+        {
+            List<Pack> packs = GetPacks();
+            List<Card> allCards = GetAllCards();
+
+            var outputs = new Dictionary<HearthDb.Enums.Rarity, List<String>>();
+            packs.GroupBy(pack => pack.UploaderId).ToList().ForEach((group) =>
+            {
+                var packsCount = group.ToList().Count();
+                WriteLine("------------------------------");
+                WriteLine("{0} - {1}", group.Key, packsCount);
+                WriteLine("------------------------------");
+                WriteLine("");
+                var allUserCards = group.Select((p) => p.Cards)
+                    .Aggregate(new List<Card>(), (result, cards) =>
+                    {
+                        result.AddRange(cards);
+                        return result;
+                    }).ToList();
+
+                var cardCount = new Dictionary<HearthDb.Card, int>();
+                allUserCards.ForEach((card) =>
+                {
+                    HearthDb.Card key = card.CardInfo;
+                    int currentCount = cardCount.ContainsKey(key) ? cardCount[key] : 0;
+                    currentCount++;
+                    cardCount[key] = currentCount;
+                });
+
+                var sorted = cardCount.OrderByDescending(kp => kp.Value).GroupBy(kp => kp.Key.Rarity).ToList();
+                foreach (var g in sorted)
+                {
+                    var kvps = g.ToList();
+                    var total = kvps.Sum(kp => kp.Value);
+
+                    WriteLine("{0} - {1} - {2}", g.Key, kvps.Count(), total);
+                    WriteLine("------------------------------");
+                    WriteLine("{0:0.00}% - {1:0.00}%", ((double)kvps[kvps.Count() - 1].Value / (double)total) * 100.0d, ((double)kvps[0].Value / (double)total) * 100.0d);
+
+                    if (!outputs.ContainsKey(g.Key))
+                    {
+                        outputs[g.Key] = new List<string>();
+                    }
+                    outputs[g.Key].Add(string.Format("{0:00.00}%", (double)kvps[0].Value / (double)kvps[kvps.Count() - 1].Value));
+                    //kvps.ForEach((kvp) =>
+                    //{
+                    //    WriteLine("{0,25} {1}", kvp.Key.Name, kvp.Value);
+                    //});
+                    WriteLine("");
+                }
+            });
+
+            WriteLine("------------------------------");
+            WriteLine("------------------------------");
+            WriteLine("");
+            outputs.ToList().ForEach((kvp) =>
+            {
+                WriteLine("{0} ", kvp.Key);
+                WriteLine("------------------------------");
+                kvp.Value.OrderByDescending(v => v).ToList().ForEach((v) =>
+                {
+                    WriteLine(v);
+                });
+                WriteLine("");
+            });
+        }
+
         private void Write(String format, params object[] args)
         {
             string str = string.Format(format, args);
-            Console.Write(str);
+            //Console.Write(str);
             File.AppendAllText("out.txt", str);
         }
 
@@ -84,6 +168,7 @@ namespace HSPackParser.Data.Queriers
             catch { }
 
             PrintByCardCounts();
+            PrintDupesByUser();
         }
     }
 }
